1e98c97b88bc975dac132cb8685fb98b3567c89e
Christopher League <league@contrapunctus.net>
2011-11-27 18:02:06 -0500

    Keep state in activity, implement clear button.

--- SketchBot/src/net/liucs/SketchBotActivity.java
+++ SketchBot/src/net/liucs/SketchBotActivity.java
@@ -1,13 +1,33 @@
 package net.liucs;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.graphics.Path;
 
 public class SketchBotActivity extends Activity {
-    /** Called when the activity is first created. */
+
+	/* State of app: */
+	ArrayList<Path> paths = new ArrayList<Path>();
+	SketchBotView view;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        view = (SketchBotView) findViewById(R.id.sketchBotView1);
+        view.setParentActivity(this);
+        Button clear = (Button) findViewById(R.id.clearButton);
+        clear.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				paths.clear();
+				view.invalidate();
+			}
+        });
     }
 }
--- SketchBot/src/net/liucs/SketchBotView.java
+++ SketchBot/src/net/liucs/SketchBotView.java
@@ -1,18 +1,18 @@
 package net.liucs;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class SketchBotView extends View {
 
+	private SketchBotActivity activity;
+	
 	public SketchBotView(Context context) {
 		super(context);
 	}
@@ -21,29 +21,38 @@ public class SketchBotView extends View {
 		super(context, attrs);
 	}
 	
+	public void setParentActivity(SketchBotActivity a) {
+		this.activity = a;
+	}
+	
 	private int myColor = Color.WHITE;
 	
-	private ArrayList<Point> points = new ArrayList<Point>();
-
-	private void addMotionPoints(MotionEvent event) {
-		for(int i = 0; i < event.getPointerCount(); i++) {
-			int x = (int) event.getX(i);
-			int y = (int) event.getY(i);
-			points.add(new Point(x, y));
-		}		
-	}
+//	private void addMotionPoints(MotionEvent event) {
+//		for(int i = 0; i < event.getPointerCount(); i++) {
+//			int x = (int) event.getX(i);
+//			int y = (int) event.getY(i);
+//			points.add(new Point(x, y));
+//		}		
+//	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		super.onTouchEvent(event);
 		switch(event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-		case MotionEvent.ACTION_MOVE:
-		case MotionEvent.ACTION_UP:
-			addMotionPoints(event);
+		case MotionEvent.ACTION_DOWN: {
+			Path path = new Path();
+			path.moveTo(event.getX(0), event.getY(0));
+			activity.paths.add(path);
 			invalidate();
 			return true;
-//			myColor = (myColor == Color.RED)? Color.YELLOW : Color.RED;
+		}
+		case MotionEvent.ACTION_MOVE:
+		case MotionEvent.ACTION_UP: {
+			Path path = activity.paths.get(activity.paths.size()-1);
+			path.lineTo(event.getX(0),event.getY(0));
+			invalidate();
+			return true;
+		}
 		default:
 			return false; // not handled
 		}
@@ -54,18 +63,10 @@ public class SketchBotView extends View {
 		super.draw(canvas);
 		Paint paint = new Paint();
 		paint.setColor(myColor);
-		Point prev = null;
-		for(Point p : points) {
-			if(prev != null) {
-				canvas.drawLine(prev.x, prev.y, p.x, p.y, paint);
-//				canvas.drawPoint(p.x, p.y, paint);
-			}
-			prev = p;
+		paint.setStyle(Paint.Style.STROKE);
+		for(Path p : activity.paths) {
+			canvas.drawPath(p, paint);
 		}
-//		float h = canvas.getHeight();
-//		float w = canvas.getWidth();
-//		float x = 25;
-//		canvas.drawRect(x, x, w-x, h-x, p);
 	}
 
 }
