d6577b331bb1bf4556228c0f6223eb2ca15b663a
Christopher League <league@contrapunctus.net>
2011-11-27 21:26:27 -0500

    Color buttons.

--- SketchBot/src/net/liucs/SketchBotActivity.java
+++ SketchBot/src/net/liucs/SketchBotActivity.java
@@ -6,13 +6,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.graphics.Color;
 import android.graphics.Path;
+
+class PathColorPair {
+	Path path;
+	int color;
+	PathColorPair(Path p, int c) {
+		path = p;
+		color = c;
+	}
+}
 
 public class SketchBotActivity extends Activity {
 
 	/* State of app: */
-	ArrayList<Path> paths = new ArrayList<Path>();
+	ArrayList<PathColorPair> paths = new ArrayList<PathColorPair>();
+	int currentColor = Color.WHITE;
 	SketchBotView view;
+
+	class ColorChanger implements View.OnClickListener {
+		private int targetColor;
+		ColorChanger(int c) {
+			targetColor = c;
+		}
+		@Override public void onClick(View v) {
+			currentColor = targetColor;
+		}
+	}
 	
 	/** Called when the activity is first created. */
     @Override
@@ -29,5 +50,11 @@ public class SketchBotActivity extends Activity {
 				view.invalidate();
 			}
         });
+        Button redButton = (Button) findViewById(R.id.redButton);
+        Button blueButton = (Button) findViewById(R.id.blueButton);
+        Button whiteButton = (Button) findViewById(R.id.whiteButton);
+        redButton.setOnClickListener(new ColorChanger(Color.RED));
+        blueButton.setOnClickListener(new ColorChanger(Color.BLUE));
+        whiteButton.setOnClickListener(new ColorChanger(Color.WHITE));
     }
 }
--- SketchBot/src/net/liucs/SketchBotView.java
+++ SketchBot/src/net/liucs/SketchBotView.java
@@ -25,16 +25,6 @@ public class SketchBotView extends View {
 		this.activity = a;
 	}
 	
-	private int myColor = Color.WHITE;
-	
-//	private void addMotionPoints(MotionEvent event) {
-//		for(int i = 0; i < event.getPointerCount(); i++) {
-//			int x = (int) event.getX(i);
-//			int y = (int) event.getY(i);
-//			points.add(new Point(x, y));
-//		}		
-//	}
-	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		super.onTouchEvent(event);
@@ -42,14 +32,14 @@ public class SketchBotView extends View {
 		case MotionEvent.ACTION_DOWN: {
 			Path path = new Path();
 			path.moveTo(event.getX(0), event.getY(0));
-			activity.paths.add(path);
+			activity.paths.add(new PathColorPair(path, activity.currentColor));
 			invalidate();
 			return true;
 		}
 		case MotionEvent.ACTION_MOVE:
 		case MotionEvent.ACTION_UP: {
-			Path path = activity.paths.get(activity.paths.size()-1);
-			path.lineTo(event.getX(0),event.getY(0));
+			PathColorPair pc = activity.paths.get(activity.paths.size()-1);
+			pc.path.lineTo(event.getX(0),event.getY(0));
 			invalidate();
 			return true;
 		}
@@ -62,10 +52,10 @@ public class SketchBotView extends View {
 	public void draw(Canvas canvas) {
 		super.draw(canvas);
 		Paint paint = new Paint();
-		paint.setColor(myColor);
 		paint.setStyle(Paint.Style.STROKE);
-		for(Path p : activity.paths) {
-			canvas.drawPath(p, paint);
+		for(PathColorPair pc : activity.paths) {
+			paint.setColor(pc.color);
+			canvas.drawPath(pc.path, paint);
 		}
 	}
 
