d059dbae494d84b3a1cb580394b6862863376527
Christopher League <league@contrapunctus.net>
2012-04-16 18:59:22 -0400

    toggle dice on click.

--- Yatzy/src/edu/liu/YatzyActivity.java
+++ Yatzy/src/edu/liu/YatzyActivity.java
@@ -1,14 +1,18 @@
 package edu.liu;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class YatzyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	final int NUM_DICE = 5;
+	Die[] dice = new Die[NUM_DICE];
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
@@ -16,10 +20,9 @@ public class YatzyActivity extends Activity {
         main.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout diceRow = new LinearLayout(this);
-        for(int d : onDice) {
-        	ImageView im = new ImageView(this);
-        	im.setImageResource(d);
-        	diceRow.addView(im);
+        for(int i = 0; i < NUM_DICE; i++) {
+        	dice[i] = new Die(this, i);
+        	diceRow.addView(dice[i]);
         }
         main.addView(diceRow);
         
@@ -38,4 +41,29 @@ public class YatzyActivity extends Activity {
     	R.drawable.die1off, R.drawable.die2off, R.drawable.die3off,
     	R.drawable.die4off, R.drawable.die5off, R.drawable.die6off,
     };
+    
+    class Die extends ImageView implements View.OnClickListener {
+		int value;
+    	boolean on;
+    	public Die(Context context, int value) {
+			super(context);
+			setOnClickListener(this);
+			this.value = value;
+			this.on = false;
+			setResource();
+		}
+    	void setResource() {
+    		if(on) {
+    			setImageResource(onDice[value]);
+    		}
+    		else {
+    			setImageResource(offDice[value]);
+    		}
+    	}
+		@Override
+		public void onClick(View v) {
+			this.on = !this.on;
+			setResource();
+		}
+    }
 }
